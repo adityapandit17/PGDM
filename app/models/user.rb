ddfac5517f5330fdc1fa03a332cdb11184192fec
adityapandit17 <adityapandit38@gmail.com>
2019-11-30 17:30:49 +0530

    added role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+# user model class
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :academic_session
+  enum role: %i[student faculty admin]
 end
