17fd441c01155333d8caf6b0b45420a68a920e46
adityapandit17 <adityapandit38@gmail.com>
2019-12-01 18:35:10 +0530

    added login via username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,30 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :academic_session
   enum role: %i[student faculty admin]
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    return unless User.where(email: username).exists?
+
+    errors.add(:username, :invalid)
+  end
 end
